88c3b2f45d58061f90e9b81aa2814b52d00405ae
David <daviduartedf@gmail.com>
2025-02-20 16:10:44 -0300

    fix

--- app/procedimentos/procedimentos-template.tsx
+++ app/procedimentos/procedimentos-template.tsx
@@ -40,7 +40,7 @@ export function ProcedimentosTemplate() {
       <div className="w-full flex">
         <Button variant="outline">
           <Plus />
-          Adicionar insumo
+          Adicionar controle
         </Button>
       </div>
 
